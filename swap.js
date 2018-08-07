1d08c68fe1956391697568cef4a07ec4d3cef923
chesterchenn <chesterchenn@gmail.com>
2018-08-07 22:22:37 +0800

    参数说明

--- swap.js
+++ swap.js
@@ -1,12 +1,12 @@
 /* 
- * 交换数组元素(排序)
- * @param {array} array
- * @param {number} position
- * @param {number} position
+ * 交换数组元素
+ * @param {array} 执行操作的数组
+ * @param {number} 数组的前一个位置
+ * @param {number} 数组的后一个位置
  * @return {array} 交换后的数组
  */
 
-function swap(array, prior, next) {
+var swap = function(array, prior, next) {
   var temp = array[prior]
   array[prior] = array[next]
   array[next] = temp
