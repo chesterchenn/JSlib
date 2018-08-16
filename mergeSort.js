e3b5c4da5eee7c8d31612b44641fb72a008cae65
chesterchenn <chesterchenn@gmail.com>
2018-08-16 14:27:21 +0800

    自顶向下的归并排序

--- mergeSort.js
+++ mergeSort.js
@@ -1,5 +1,9 @@
 /*
- * 原地归并的抽象方法
+ * 归并排序
+ */
+
+ /*
+ * 原地归并的方法
  * @param {array} 要归并的数组
  * @param {number} 数组起始位置
  * @param {number} 数组中间位置
@@ -16,5 +20,23 @@ var merge = function(arr, low, mid, high) {
     else if (arrCopy[i] < arrCopy[j]) arr[k] = arrCopy[i++]  // 左半边的当前元素小于右半边的当前元素(取左半边的当前元素)
     else                              arr[k] = arrCopy[j++]  // 右半边的当前元素小于左半边的当前元素(取右半边的当前元素)
   }
+  return arr
+}
+
+/*
+ * 自顶向下的归并排序
+ * @param {array} 进行排序的数组
+ * @return {array} 排序后的数组
+ */
+
+var MergeSort = function(arr) {
+  var sort = function(a, low, high) {
+    if (high <= low) return;
+    var mid = low + (~~((high - low) / 2))
+    sort(a, low, mid)         // 将左半边排序
+    sort(a, mid + 1, high)    // 将右半边排序
+    merge(a, low, mid, high)  // 归并结果
+  }
+  sort(arr, 0, arr.length - 1)
   return arr
 }
