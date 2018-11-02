452d11d5d8f31560b86b7366ba2d8021465cf97d
chesterchenn <chesterchenn@gmail.com>
2018-11-02 11:22:57 +0800

    插入排序

--- insertionSort.js
+++ insertionSort.js
@@ -1,16 +1,15 @@
 import swap from './swap'
-/*
+/**
  * 插入排序
- * @param {array} 数组
- * @return {array} 数组
+ * @param {Array} arr 需要进行排序的数组
  */
 
 var insertionSort = function(arr) {
-  var len = arr.length
+  var len = arr.length;
   for (var i = 1; i < len; i++) {
     for (var j = i; j > 0 && arr[j] < arr[j-1]; j--) {
-      swap(arr, j, j-1)
+      swap(arr, j, j-1);
     }
   }
-  return arr
+  return;
 }
