8721f8872b8902dfe1c1bc9a17a9e77a3400dad3
chesterchenn <chesterchenn@gmail.com>
2018-11-02 13:50:49 +0800

    选择排序

--- selectionSort.js
+++ selectionSort.js
@@ -1,20 +1,19 @@
 import swap from './swap'
-/*
+/**
  * 选择排序
- * @param {array} 数组
- * @return {array} 数组
+ * @param {Array} arr 数组
  */
 
 var selectionSort = function(arr) {
-  var len = arr.length, min
+  var len = arr.length, min;
   for(var i = 0; i < len; i++) {
-    min = i
+    minIndex = i;
     for(var j = i + 1; j < len; j++) {
-      if (arr[j] < arr[min]) {
-        min = j
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
       }
     }
-    swap(arr, i, min)
+    swap(arr, i, minIndex);
   }
-  return arr
+  return;
 }
