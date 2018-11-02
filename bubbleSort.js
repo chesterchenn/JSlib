16c56521c3408d37164c5e4b77384c771c82f4ba
chesterchenn <chesterchenn@gmail.com>
2018-11-02 11:12:00 +0800

    冒泡排序

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,55 +1,47 @@
 import swap from './swap'
-/*
+/**
  * 冒泡排序
- * 稳定排序，时间复杂度O(n^2)
- * @param {array} 数组
- * @return {array} 数组
+ * @param {Array} arr 需要进行排序的数组
  */
 
-/*
- * 冒泡排序的简易用法
- */
-var BubbleSort = function(array) {
-  var len = array.length
+// 冒泡排序的简易用法
+var BubbleSort = function(arr) {
+  var len = arr.length
   for(var i = 0; i < len - 1; i++) {
     for (var j = i + 1; j < len; j++) {
-      if (array[i] > array[j]) {
-        array = swap(array, i, j)
+      if (arr[i] > arr[j]) {
+        arr = swap(arr, i, j)
       }
     }
   }
-  return array
+  return;
 }
 
-/*
- * 标准的冒泡排序
- */
-var BubbleSort = function(array) {
-  var len = array.length
+// 标准的冒泡排序
+var BubbleSort = function(arr) {
+  var len = arr.length
   for (var i = 0; i < len - 1; i++) {
     for (var j = len - 1; j >= i; j--) {
-      if (array[j] > array[j+1]) {
-        array = swap(array, j, j+1)
+      if (arr[j] > arr[j+1]) {
+        arr = swap(arr, j, j+1)
       }
     }
   }
-  return array
+  return;
 }
 
-/*
- * 增加flag
- */
-var BubbleSort = function(array) {
-  var len = array.length
+// 增加flag
+var BubbleSort = function(arr) {
+  var len = arr.length
   var flag = true
   for (var i = 0; i < (len - 1) && flag; i++) {
     flag = false
     for (var j = len - 1; j >= i; j--) {
-      if (array[j] > array[j+1]) {
-        array = swap(array, j, j+1)
+      if (arr[j] > arr[j+1]) {
+        arr = swap(arr, j, j+1)
         flag = true
       }
     }
   }
-  return array
+  return;
 }
