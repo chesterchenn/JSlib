c5cbea75db17981117b207730d403e2452875584
chesterchenn <chesterchenn@gmail.com>
2018-11-02 13:54:33 +0800

    交换数组元素

--- swap.js
+++ swap.js
@@ -1,14 +1,14 @@
-/* 
+/** 
  * 交换数组元素
- * @param {array} 执行操作的数组
- * @param {number} 数组的前一个位置
- * @param {number} 数组的后一个位置
- * @return {array} 交换后的数组
+ * @param {Array} arr 执行操作的数组
+ * @param {Number} prior 数组的前一个位置
+ * @param {Number} next 数组的后一个位置
+ * @return {Array} 交换后的数组
  */
 
-var swap = function(array, prior, next) {
-  var temp = array[prior]
-  array[prior] = array[next]
-  array[next] = temp
-  return array
+var swap = function(arr, prior, next) {
+  var temp = arr[prior]
+  arr[prior] = arr[next]
+  arr[next] = temp
+  return arr
 }
