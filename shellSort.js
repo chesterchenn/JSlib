be19030d443d1c7619f6d719bfb9cdf9bb92e6ea
chesterchenn <chesterchenn@gmail.com>
2018-11-02 10:06:47 +0800

    希尔排序

--- shellSort.js
+++ shellSort.js
@@ -5,8 +5,9 @@
 
 var shellSort = function(arr) {
   var len = arr.length;
-  // 增量序列的递减
+  // 设置增量gap,并逐步递减增量
   for (var gap = ~~(len / 2); gap > 0; gap = ~~(gap / 2)) {
+    // 从第gap个元素，逐个对其所在组进行直接插入排序操作
     for (var i = gap; i < len; i += 1) {
       var temp = arr[i];
       for (var j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
