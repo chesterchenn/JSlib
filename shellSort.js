30add6ec6cc522bf4af02bbc110c6992666540ab
chesterchenn <chesterchenn@gmail.com>
2018-11-02 11:03:00 +0800

    希尔排序

--- shellSort.js
+++ shellSort.js
@@ -1,5 +1,29 @@
+import swap from './swap';
+
 /**
- * 希尔排序 ShellSort
+ * 希尔排序 ShellSort 针对有序序列在插入时采用交换法
+ * @param {Array} arr 进行排序的数组
+ */
+var shellSort = function(arr) {
+  var len = arr.length;
+  // 设置增量gap,并逐步递减增量
+  for (var gap = ~~(len / 2); gap > 0; gap = ~~(gap / 2)) {
+    // 从第gap个元素，逐个对其所在组进行直接插入排序操作
+    for (var i = gap; i < len; i += 1) {
+      var minIndex = i;
+      while (minIndex >= gap && arr[minIndex] < arr[minIndex - gap]) {
+        // 插入排序采用交换法
+        swap(arr, minIndex, minIndex - gap);
+        minIndex -= gap;
+      }
+    }
+  }
+  return;
+}
+
+
+/**
+ * 希尔排序 ShellSort 针对有序序列在插入时采用移动法
  * @param {Array} arr 进行排序的数组
  */
 
@@ -11,6 +35,7 @@ var shellSort = function(arr) {
     for (var i = gap; i < len; i += 1) {
       var temp = arr[i];
       for (var j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+        // 移动法
         arr[j] = arr[j - gap];
       }
       arr[j] = temp;
