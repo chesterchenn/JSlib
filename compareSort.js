821aa77fbf753109aaab5a7026840b4f0e144c69
chesterchenn <chesterchenn@gmail.com>
2018-11-05 15:24:43 +0800

    比较数组排序耗时

--- compareSort.js
+++ compareSort.js
@@ -1,29 +1,24 @@
-import randomInt from './randomInteger'
-// moment: https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js
+import randomInt from './randomInt';
+import moment from 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js';
 
-/*
- * 用于比较数组排序耗时, 需要引入moment以及测试的排序
+/**
+ * 比较数组排序耗时
  * @param {number} 循环测试次数
- * @param {number} 指定生成的个数, 默认为10
- * @param {number} 生成数的范围，默认为100
- * @param {boolean} 生成的数字是否不重复，默认为否
  */
 
-var compareSort = function(loop = 5, num, range, flag) {
-  var start, end
+var compareSort = function(loop = 3) {
+  var start, end;
 
   for (var i = 0; i < loop; i++) {
-    start = moment().valueOf()
-    var arr = randomInt(num, range, flag)
-    end = moment().valueOf()
-    console.log('生成数组：', end - start, 'ms',)
-    
-    // var arrCopy = arr.slice() 拷贝数组
+    start = moment().valueOf();
+    var arr = randomInt();                        // 可指定个数，范围，是否重复
+    var arrCopy = arr.slice();                    // 拷贝数组
+    end = moment().valueOf();
+    console.log('生成数组：', end - start, 'ms');
 
-    // start = moment().valueOf()
+    // start = moment().valueOf();
     // 排序
-    // end = moment().valueOf()
-    // console.log('排序名称：', end - start, 'ms')
-
+    // end = moment().valueOf();
+    // console.log('排序名称：', end - start, 'ms');
   }
 }
