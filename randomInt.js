0403405ed1edcc995d832aa7fd85a47eb2922e2b
chesterchenn <chesterchenn@gmail.com>
2018-11-05 15:15:36 +0800

    随机生成整数

---
+++ randomInt.js
@@ -0,0 +1,28 @@
+/**
+ * 随机生成整数
+ * @param {number} count 生成的个数, 默认为10
+ * @param {number} range 生成数的范围，默认为100
+ * @param {boolean} flag 生成的数字是否允许重复，默认为否
+ * @return {Array} 返回生成的整数组
+ */
+
+var randomInt = function(count = 10, range = 100, flag = false) {
+  try {
+    if (flag && count > range) {
+      throw '在指定不重复条件下，指定的个数不得超出范围数'
+    }
+    
+    var arr = [], i = 0
+    while(i < count) {
+      var val = ~~(Math.random() * range)
+      if (flag || !(flag || arr.includes(val))) {
+        arr.push(val);
+        i++;
+      }
+    }
+    return arr
+  }
+  catch (e) {
+    console.error(e)
+  }
+}
