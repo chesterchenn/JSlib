6d71ad4ddd6b52323de5053fb4c676bca1889ebc
chesterchenn <chesterchenn@gmail.com>
2018-11-05 14:20:52 +0800

    快速排序

--- quickSort.js
+++ quickSort.js
@@ -1,36 +1,61 @@
-/*
- * 快速排序
- * @param {array} 要排序的数组
- * @return {array} 排序后的数组
+import swap from './swap';
+
+/**
+ * 快速排序 
+ * quickSort(arr, 0, arr.length - 1)
+ * @param {Array} arr 进行排序的数组
+ * @param {number} lo 起始位置
+ * @param {number} hi 结束位置
  */ 
-var quickSort = function(arr) {
-  var sort = function(a, low, high) {
-    if (high <= low) return;
-    var j = partition(a, low, high)
-    sort(a, low, j - 1)        // 对左半部排序
-    sort(a, j + 1, high)       // 对右半部排序 
+var quickSort = function(arr, lo, hi) {
+  if (lo < hi) {
+    var p = partition(arr, lo, hi);
+    quickSort(arr, lo, p - 1);        // 对左半部排序
+    quickSort(arr, p + 1, hi);        // 对右半部排序
   }
-  sort(arr, 0, arr.length - 1)
-  return arr
+  return;
 }
 
-/*
+
+/**
  * 快速排序的切分
- * @param {array} 切分的数组
- * @param {number} 切分数组的起始位置
- * @param {number} 切分数组的结束位置
+ * @param {Array} arr 切分的数组
+ * @param {number} lo 切分数组的起始位置
+ * @param {number} hi 切分数组的结束位置
  * @return {number} 返回切分位置
  */
 
-var partition = function(arr, low, high) {
-  var i = low, j = high + 1      // 左右扫描指针
-  var compare = arr[low]         // 切分元素
-  while(true) {
-    while(arr[++i] < compare) if (i === high) break
-    while(compare < arr[--j]) if (j === low) break
-    if (i >= j) break
-    swap(arr, i, j) 
+// 左右指针，从左右两边往中间扫描
+// 枢轴元素指定为左边第一个元素
+var partition = function(arr, lo, hi) {            
+  var pivot = arr[lo];           // 枢轴元素
+  while(lo < hi) {
+    while(lo < hi && arr[hi] >= pivot) hi--;
+    if (lo < hi) {
+      arr[lo++] = arr[hi];
+    }
+    while(lo < hi && arr[lo] <= pivot) lo++;
+    if (lo < hi) {
+      arr[hi--] = arr[lo];
+    }
   }
-  swap(arr, low, j)              // 将切分元素放入正确的位置
-  return j                       // 达成 arr[lo...j-1] <= arr[j] <= arr[j+1...hi]
+  arr[lo] = pivot;                 // 将枢轴元素放入正确的位置
+  return hi;
+}
+
+// 指针从左往右扫描
+// 枢轴元素指定为右边第一个元素
+var partition = function(arr, lo, hi) {
+  var pivot = arr[hi];
+  var i = lo;
+  for (var j = lo; j < hi; j++) {
+    if (arr[j] < pivot) {
+      if (i !== j) {
+        swap(arr, i, j)
+      }
+      i++;
+    }
+  }
+  swap(arr, i, j);               // 将枢轴元素放入正确的位置
+  return i;
 }
