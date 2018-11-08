1ae3e8506a8f43f749bc7cf0496efbf3b6e316ad
chesterchenn <chesterchenn@gmail.com>
2018-11-08 09:55:33 +0800

    最大公约数

--- gcd.js
+++ gcd.js
@@ -1,12 +1,12 @@
-/*
+/**
  * 辗转相除法得出最大公约数
- * @param {number} p
- * @param {number} q
- * @return {number} result
+ * @param {number} p 整数
+ * @param {number} q 整数
+ * @return {number} 最大公约数
  */
 
 function gcd(p, q) {
-  if (q == 0) return p
+  if (q === 0) return p
   var r = p % q
   return gcd(q, r)
 }
