a0a04749ba92925ff1270e1e0cf2ce687b91acf4
chesterchenn <chesterchenn@gmail.com>
2018-11-09 16:14:54 +0800

    _转驼峰

--- _2camelCase.js
+++ _2camelCase.js
@@ -1,16 +1,21 @@
-/*
- * 将_命名的字符串转化成驼峰法,非字符串直接返回原先
- * @param {string} _字符串
- * @return {string} 驼峰法字符串
+/**
+ * 将_命名的字符串转化成驼峰法
+ * @param {string} str 带_字符串
+ * @return 驼峰字符串
  */
 function _2camelCase(str) {
-  if (typeof str !== 'string') {
-    return str
+  try {
+    if (typeof str !== 'string') {
+      throw 'This is not String.';
+    }
+
+    var temp = str.split("_");
+    for (var i = 1, len = temp.length; i < len; i++) {
+      temp[i] = temp[i][0].toUpperCase() + temp[i].substring(1);
+    }
+    var result = temp.join('')
+    return result;
+  } catch (error) {
+    console.error(error)
   }
-  var temp = str.split("_")
-  for (var i = 1, len = temp.length; i < len; i++) {
-    temp[i] = temp[i][0].toUpperCase() + temp[i].substring(1)
-  }
-  var result = temp.join('')
-  return result
 }
