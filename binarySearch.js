0c3cefbe4049badeb348029c51dc37331910b779
chesterchenn <chesterchenn@gmail.com>
2018-11-09 16:04:18 +0800

    二分查找法

--- binarySearch.js
+++ binarySearch.js
@@ -1,24 +1,22 @@
-/*
+/**
  * 二分查找/折半查找
- * 只针对有序排列, 时间复杂度O(logn)
- * @param {array} arr被检索的数组
- * @key {number} key检索的值
- * @return {number,boolean} position or false
+ * @param {Array} arr 被检索的数组
+ * @param {number} key 检索的值
+ * @return 若检索到，返回相应的位置，否则返回false
  */
 
 function binarySearch(arr, key) {
-  var low = 0
-  var high = arr.length - 1
-  var mid
+  var low = 0, high = arr.length - 1;
+  var mid;
   while (low <= high) {
-    mid = Math.ceil((low + high) / 2)
+    mid = low + ~~((high - low) / 2);       // 防止溢出，等同(low + high) / 2
     if (key < arr[mid]) {
-      high = mid - 1
+      high = mid - 1;
     } else if (key > arr[mid]) {
-      low = mid + 1
+      low = mid + 1;
     } else {
-      return mid
+      return mid;
     }
   }
-  return false
+  return false;
 }
