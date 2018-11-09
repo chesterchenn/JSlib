7d37f904b448bc5d1be61c6937f7190696d4e829
chesterchenn <chesterchenn@gmail.com>
2018-11-09 15:12:12 +0800

    堆排序

--- heapSort.js
+++ heapSort.js
@@ -1,51 +1,65 @@
-var heapify = function(arr, n, i) {
-  var largest = i;
-  var l = 2 * i + 1;
-  var r = 2 * i + 2;
-  if (l < n && arr[l] > arr[largest]) {
-    largest = l;
-  }
-  if (r < n && arr[r] > arr[largest]) {
-    largest = r;
-  }
-  if (largest != i) {
-    var ele = arr[i];
-    arr[i] = arr[largest];
-    arr[largest] = ele;
-    heapify(arr, n, largest)
-  }
-}
-
-var heapify = function(arr, i, n) {
-  var temp = arr[i];
-  for (var j = 2 * i + 1; i <= n; j = j * 2 + 1) {
-    if (j < n && arr[j] < arr[j+1]) ++j;
-    if (!(temp < arr[j])) break;
-    arr[i] = arr[j]
-    i = j
-  }
-  arr[i] = temp;
-}
-
-var buildHeap = function(arr, n) {
-  for (var i = ~~(n / 2) - 1;i >= 0; i--) {
-    heapify(arr, i, n);
-  }
-}
-
-
-var heapSort = function(arr, n) {
-  buildHeap(arr, n)
-  // console.log(arr)
-  for (var i = n; i > 0; i--) {
-    var temp = arr[0];
+/**
+ * 堆排序
+ * @param {Array} arr 进行排序的数组
+ */
+var heapSort = function(arr) {
+  buildHeap(arr, arr.length - 1)                // 建立最大堆
+  for (var i = arr.length - 1; i > 0; i--) {
+    var temp = arr[0];                          // 交换最大堆的根节点和尾节点
     arr[0] = arr[i];
     arr[i] = temp;
-    console.log(arr)
-    heapify(arr, 0, i - 1);
+    heapify(arr, 0, i - 1);                     // 调整最大堆arr[0, i-1]
   }
 }
 
-var arr = [1, 3, 2, 4, 6, 7]
-heapSort(arr, arr.length - 1)
-console.log(arr);
+/**
+ * 建立最大堆MaxHeap
+ * @param {Array} arr 要建立最大堆的数组
+ * @param {number} hi 建立最大堆的结束位置
+ */
+var buildHeap = function(arr, hi) {
+  for (var i = ~~(hi / 2 - 1);i >= 0; i--) {
+    heapify(arr, i, hi);
+  }
+}
+
+/**
+ * 最大堆调整heapify, 递归法
+ * @param {Arra} arr 要进行调整的最大堆
+ * @param {*} lo 调整最大堆的起始位置
+ * @param {*} hi 调整最大堆的结束位置
+ */
+var heapify = function(arr, lo, hi) {
+  var maxIndex = lo;
+  var l = 2 * lo + 1;
+  var r = 2 * lo + 2;
+  if (l <= hi && arr[l] > arr[maxIndex]) {
+    maxIndex = l;
+  }
+  if (r <= hi && arr[r] > arr[maxIndex]) {
+    maxIndex = r;
+  }
+  if (maxIndex != lo) {
+    var temp = arr[lo];
+    arr[lo] = arr[maxIndex];
+    arr[maxIndex] = temp;
+    heapify(arr, maxIndex, hi);
+  }
+}
+
+/**
+ * 最大堆调整heapify, 循环迭代法
+ * @param {Arra} arr 要进行调整的最大堆
+ * @param {*} lo 调整最大堆的起始位置
+ * @param {*} hi 调整最大堆的结束位置
+ */
+var heapify = function(arr, lo, hi) {
+  var temp = arr[lo];
+  for (var j = 2 * lo + 1; j <= hi; j = j * 2 + 1) {
+    if (j < n && arr[j] < arr[j+1]) j++;
+    if (!(temp < arr[j])) break;
+    arr[lo] = arr[j]
+    lo = j
+  }
+  arr[lo] = temp;
+}
