ac3aef381f9d08c1e427f0ba88ee81c5b9cbf58d
chesterchenn <chesterchenn@gmail.com>
2021-12-03 22:46:02 +0800

    feat: 部分题目

---
+++ topic/fibonacci.js
@@ -0,0 +1,12 @@
+/**
+ * 求斐波那契数列第 n 项的值，1、1、2、3、5、8、13、21、34...。
+ * 定义如下：F(1)=1，F(2)=1, F(n)=F(n - 1)+F(n - 2)（n ≥ 3，n ∈ N*）
+ * @param {number} n 第 n 项
+ * @return {number} 斐波那契数列第 n 项的值
+ */
+
+// 递归，时间复杂度高，待优化
+const fibonacci = function (n) {
+  if (n == 1 || n == 2) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+};
