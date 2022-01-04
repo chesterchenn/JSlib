e8ebfc3d486e7fb9972d06a470859494e9848509
chesterchenn <chesterchenn@outlook.com>
2022-01-04 17:56:43 +0800

    feat: 补充负数

--- format.js
+++ format.js
@@ -2,12 +2,18 @@
  * 格式化千分位
  */
 function formatqty(number, qty) {
-  const i = `${parseInt(String(number), 10)}`;
-  if (isNaN(i)) return number;
+  const i = `${Math.abs(parseInt(String(number), 10))}`;
+  if (isNaN(Number(i))) return number;
 
-  const tail = String(number).slice(i.length);
+  const negative = +number < 0 ? '-' : '';
+  const tail = String(Math.abs(number)).slice(i.length);
   let j = i.length > 3 ? i.length % 3 : 0;
-  return (j ? i.substr(0, j) + qty : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, `$1${qty}`) + tail;
+  return (
+    negative +
+    (j ? i.substr(0, j) + qty : '') +
+    i.substr(j).replace(/(\d{3})(?=\d)/g, `$1${qty}`) +
+    tail
+  );
 }
 
 console.log(formatqty(12345.01, ','));
@@ -16,6 +22,10 @@ console.log(formatqty(12345678, ','));
 console.log(formatqty(123, ','));
 console.log(formatqty(0, ','));
 console.log(formatqty(0.123, ','));
+console.log(formatqty(-0.123, ','));
+console.log(formatqty(-123, ','));
+console.log(formatqty(-12345.01, ','));
+console.log(formatqty(-12345678, ','));
 console.log(formatqty('', ','));
 console.log(formatqty('1234', ','));
 console.log(formatqty('abc', ','));
